337cac9ba5f76283178ed38d173a2253b46400dc
Kaze66 <juliocesarojas13@gmail.com>
2024-10-14 17:13:04 +0000

    mision3

--- tipos de datos/funciones.py
+++ tipos de datos/funciones.py
@@ -1,2 +1,16 @@
-def saludar():
-    print("Hola mundo")
+def calcular(x, y):
+    return x * y
+
+subtotal = 0
+cantidad = 1
+
+while cantidad != 0:
+    print("Ingrese la cantidad del producto:")
+    cantidad = float(input())
+    if cantidad == 0:
+        break
+    print("Ingrese el valor del producto:")
+    valor = float(input())
+    total = calcular(cantidad, valor)
+    subtotal += total
+print("El total de los productos es: {subtotal}")
