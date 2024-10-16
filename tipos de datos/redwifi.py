db79bb0de8de9d2792ec7df7fa4da3006782d275
Kaze66 <juliocesarojas13@gmail.com>
2024-10-16 14:30:38 +0000

    mision3

--- tipos de datos/redwifi.py
+++ tipos de datos/redwifi.py
@@ -5,6 +5,7 @@ while True:
     print("=====Elige una opcion=====")
     print("Opcion 1: Agregar wifi")
     print("Opcion 2: Ver Wifi")
+    print("Opcion 3: Salir")
     print("==========================")
     opcion=int(input("Elige la opcion:"))
     print("==========================")
@@ -21,3 +22,5 @@ while True:
     elif opcion==2:
         for i in range(len(datos)):
             print(datos[i])
+    elif opcion== 3:
+        break
